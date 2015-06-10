e81dbce042fe0d29a036739e84abc4b90baf9033
Andika Wasisto <me@awasisto.com>
2015-06-10 19:42:25 +0700

    Update

---
+++ app/src/main/java/com/awasisto/doppro/VehicleView.java
@@ -0,0 +1,13 @@
+package com.awasisto.doppro;
+
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.ImageView;
+
+public abstract class VehicleView extends ImageView {
+
+
+    public VehicleView(Context context) {
+        super(context);
+    }
+}
